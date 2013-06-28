bb9106d4b24113335535bbb7e05978ec7431e7fb
Kamie Myers <kmyers@shortstravel.com>
2013-06-28 17:30:12 -0400

    STM-737: New columns in the Searches table that pertain to air policy window and the forthcoming city codes.

---
+++ _scripts/sql/book_Searches_06282013.sql
@@ -0,0 +1,15 @@
+USE [book]
+GO
+
+ALTER TABLE [Searches]
+ADD
+	CarPickup_DateTimeActual VARCHAR(20) NULL
+	,CarDropoff_DateTimeActual VARCHAR(20) NULL
+	,Depart_DateTimeActual VARCHAR(20) NULL
+	,Depart_DateTimeStart DATETIME NULL
+	,Depart_DateTimeEnd DATETIME NULL
+	,Arrival_DateTimeActual VARCHAR(20) NULL
+	,Arrival_DateTimeStart DATETIME NULL
+	,Arrival_DateTimeEnd DATETIME NULL
+	,Air_CityCode BIT NULL
+GO
